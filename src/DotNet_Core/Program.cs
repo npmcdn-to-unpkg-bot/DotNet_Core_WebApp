dd0fe45b9f987738d93882bdec7abea2cd9702e9
LiuHuiFeng <liuhf939@hotmail.com>
2016-06-03 22:44:47 +0800

    aaa

--- src/DotNet_Core/Program.cs
+++ src/DotNet_Core/Program.cs
@@ -14,6 +14,7 @@ namespace DotNet_Core
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
